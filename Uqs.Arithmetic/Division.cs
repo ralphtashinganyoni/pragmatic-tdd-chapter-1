a8aebaa0ed21173ec53e9e9fd0756403dd499778
Ralph <ralph@chikwamapay.com>
2024-06-22 23:53:18 +0200

    Initial commit

---
+++ Uqs.Arithmetic/Division.cs
@@ -0,0 +1,10 @@
+﻿namespace Uqs.Arithmetic;
+
+public class Division
+{
+    public static decimal Divide(int dividend, int divisor)
+    {
+        decimal result = (decimal)dividend / divisor;
+        return result;
+    }
+}
